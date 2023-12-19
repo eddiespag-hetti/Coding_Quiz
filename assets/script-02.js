a7717ecc65e056708520a9ff506e602bbfaa98b8
Eddie Payne <eddie_p89@hotmail.com>
2023-12-19 18:19:38 +1100

    Refracted original code and added another script.js file

---
+++ assets/script-02.js
@@ -0,0 +1,2 @@
+// Need to make a function that takes player score from local storage and displays it on the page
+// Need to add function to Clear button to clear scores and remove from localStorage
--- assets/script.js
+++ assets/script.js
@@ -1,80 +1,7 @@
-var index = -1
+var index = 0
+var timerInterval
 
-// Countdown timer 
-var timerEl = document.getElementById('timer');
-var secondsLeft = 45;
-
-function setTime() {
-    var timeLeft = 45;
-
-    var timerInterval = setInterval(function () {
-        secondsLeft--;
-        timerEl.textContent = secondsLeft;
-
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-
-            sendMessage();
-        }
-
-    }, 1000);
-};
-
-
-function sendMessage() {
-    timerEl.textContent = "Times Up!";
-}
-
-
-
-
-
-
-// Starting the game function 
-
-var startGameEl = document.querySelector(".btn-start");
-
-function startGame(event) {
-    showQuestions()
-    setTime()
-    document.querySelector(".questions-para").style = "display: none";
-    document.querySelector(".btn-start").style = "display: none";
-    document.querySelector(".rules-para").style = "display: none";
-    document.querySelector(".quiz-questions").style = "display: block";
-
-    document.querySelector("#btn-1").addEventListener("click", showQuestions);
-    document.querySelector("#btn-2").addEventListener("click", showQuestions)
-    document.querySelector("#btn-3").addEventListener("click", showQuestions)
-    document.querySelector("#btn-4").addEventListener("click", showQuestions)
-}
-startGameEl.addEventListener('click', startGame);
-
-
-// Quiz Questions
-function showQuestions(event) {
-    if(index!=-1){
-        console.log(event.target.innerHTML)
-        if(event.target.innerHTML != questions[index].correctanswer){
-            secondsLeft-=10
-        }
-    }
-    
-    index++
-    if(index==questions.length){
-        console.log("test")
-        document.querySelector(".quiz-questions").style = "display: none";
-        document.querySelector(".initials-score").style = "display: block";
-        return
-    }
-    document.querySelector(".question").textContent = questions[index].question;
-    document.querySelector("#btn-1").textContent = questions[index].answer1;
-    document.querySelector("#btn-2").textContent = questions[index].answer2;
-    document.querySelector("#btn-3").textContent = questions[index].answer3;
-    document.querySelector("#btn-4").textContent = questions[index].answer4;
-
-}
-
-
+// Multiple choice questions array
 var questions = [
     {
         question: "What does JS stand for?",
@@ -85,20 +12,153 @@ var questions = [
         correctanswer: "JavaScript"
     }, {
         question: "What does CSS stand for?",
-        answer1: "Cascading Style Sheets",
-        answer2: "Cats Chase Sheep",
-        answer3: "Cool Cats Slide",
+        answer1: "Cats Chase Sheep",
+        answer2: "Cool Cats Slide",
+        answer3: "Cascading Style Sheets",
         answer4: "Code Cheat Sheet",
         correctanswer: "Cascading Style Sheets",
     }, {
         question: "What does HTML stand for?",
-        answer1: "HyperText Markup Language",
-        answer2: "Hide My Lunch",
+        answer1: "Hide My Lunch",
+        answer2: "HyperText Markup Language",
         answer3: "HyperText Markdown Language",
         answer4: "Hitmonlee",
         correctanswer: "HyperText Markup Language",
+    }, {
+        question: "How do you log something to the console?",
+        answer1: "console.logged()",
+        answer2: "log.console()",
+        answer3: "console.log()",
+        answer4: ".console(log)",
+        correctanswer: "console.log()",
+    }, {
+        question: "Which of the following is an example of version-control?",
+        answer1: "mit",
+        answer2: "git",
+        answer3: "wit",
+        answer4: "fit",
+        correctanswer: "git",
     }
 ]
+
+// Global Variables
+var startGameEl = document.querySelector(".btn-start");
+var timerEl = document.getElementById('timer');
+var secondsLeft = 45;
+
+var scoresArray = JSON.parse(localStorage.getItem('high-scores')) || [];
+var welcome = document.querySelector("#welcome")
+var qContainer = document.querySelector(".quiz-questions")
+var userScore = document.querySelector('.player-score');
+
+// Countdown timer function
+function setTime() {
+    var timeLeft = 45;
+    timerInterval = setInterval(function () {
+        secondsLeft--;
+        timerEl.textContent = secondsLeft;
+        if (secondsLeft === 0) {
+            showScore()
+        }
+    }, 1000);
+};
+
+
+// Starting the game function 
+function startGame(event) {
+    setTime()
+    welcome.classList.add('hideContainers');
+
+    qContainer.classList.remove('hideContainers')
+
+    showQuestions()
+}
+
+// Display the questions function
+function showQuestions() {
+
+    document.querySelector(".question").textContent = questions[index].question;
+    document.querySelector("#btn-1").textContent = questions[index].answer1;
+    document.querySelector("#btn-2").textContent = questions[index].answer2;
+    document.querySelector("#btn-3").textContent = questions[index].answer3;
+    document.querySelector("#btn-4").textContent = questions[index].answer4;
+
+}
+
+
+// Quiz Questions
+function buttonClick(event) {
+    // if/else conditional
+    if (event.target.textContent !== questions[index].correctanswer) {
+        secondsLeft -= 10
+        timerEl.textContent = secondsLeft;
+    }
+    index++
+    if (index === questions.length) {
+        showScore()
+    }
+    else {
+        showQuestions()
+    }
+}
+
+// Show the players score function
+function showScore() {
+    // display the score. hide the qContainer, show the score container. and stop the timer
+    clearInterval(timerInterval);
+    qContainer.classList.add('hideContainers')
+    userScore.classList.remove('hideContainers')
+}
+
+
+
+// Save Score function
+function saveScore() {
+    // // Using localStorage to save player initials and highscore
+    var userInput = document.querySelector('#user').value
+    var userScore = {
+        user: userInput,
+        score: secondsLeft,
+    }
+    scoresArray.push(userScore)
+
+    localStorage.setItem('high-scores', JSON.stringify(scoresArray))
+    window.location.href = 'index-02.html'
+}
+
+// Button Event listeners
+startGameEl.addEventListener("click", startGame);
+
+document.querySelector("#btn-1").addEventListener("click", buttonClick);
+document.querySelector("#btn-2").addEventListener("click", buttonClick);
+document.querySelector("#btn-3").addEventListener("click", buttonClick);
+document.querySelector("#btn-4").addEventListener("click", buttonClick);
+document.querySelector(".save-btn").addEventListener('click', saveScore);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
